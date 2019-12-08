4172ab8198e9b4f1a865922cfd73f3ff487a811a
romart333 <njuvastik@mail.ru>
2019-12-08 07:26:13 -0800

    Create project with CoreData model

---
+++ TinkoffNews/TinkoffNews/CoreData/Article+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Article+CoreDataClass.swift
+//  TinkoffNews
+//
+//  Created by Roman on 08/12/2019.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Article)
+public class Article: NSManagedObject {
+
+}
---
+++ TinkoffNews/TinkoffNews/CoreData/Article+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Article+CoreDataProperties.swift
+//  TinkoffNews
+//
+//  Created by Roman on 08/12/2019.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Article {
+
+    @nonobjc public class func getArticlefetchRequest() -> NSFetchRequest<Article> {
+        return NSFetchRequest<Article>(entityName: "Article")
+    }
+
+    @NSManaged public var countOfRedirects: Int32
+    @NSManaged public var title: String?
+    @NSManaged public var text: String?
+    @NSManaged public var slug: String?
+
+}
